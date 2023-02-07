a5ef5342c54e032cb032b022d2630f901846344d
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2023-02-07 14:49:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from pylatex import *
+
+doc = Document(documentclass = 'article')
+def generate_header(title):
+    geometry_options = {"margin": "0.7in"}
+    doc = Document(geometry_options=geometry_options)
+    # Add document header
+    header = PageStyle("header")
+    # Create left header
+    with header.create(Head("L")):
+        header.append("Name: ")
+    # Create center header
+    with header.create(Head("C")):
+        header.append("Date:")
+    # Create right header
+    with header.create(Head("R")):
+        header.append("Period:")
+    # Create left footer
+    with header.create(Foot("L")):
+        header.append("Left Footer")
+    # Create center footer
+    with header.create(Foot("C")):
+        header.append("Center Footer")
+    # Create right footer
+    with header.create(Foot("R")):
+        header.append("Right Footer")
+
+    doc.preamble.append(header)
+    doc.change_document_style("header")
+
+    # Add Heading
+    with doc.create(MiniPage(align='c')):
+        doc.append(LargeText(bold(title)))
+        doc.append(LineBreak())
+
+print("Enter the title of the test: ")
+title = input()
+generate_header(title)
+doc.generate_tex("header")
